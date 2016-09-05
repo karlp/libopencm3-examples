910642ac47e7f8d14f204087f61114155d6483ed
Karl Palsson <karlp@tweak.net.au>
2016-09-05 23:36:02 +0000

    f1 hacking.  similar to f4 but worse.
    RX is still beautiful and solid.  but tx is even shittier than on f4.
    clearly more to it than just how nak is implemented.

---
+++ examples/stm32/f1/other/usb_to_serial_rs485/usb_cdcacm-arch.c
@@ -0,0 +1,136 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2014 Karl Palsson <karlp@tweak.net.au>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/stm32/dma.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/usart.h>
+
+#include "usb_cdcacm.h"
+#include "syscfg.h"
+#include "ringb.h"
+
+extern bool out_in_progress;
+
+void usb_cdcacm_setup_pre_arch(void)
+{
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_OTGFS);
+
+#if (defined(STM32F1))
+	/*
+         * Vile hack to reenumerate, physically _drag_ d+ low.
+         * do NOT do this if you're board has proper usb pull up control!
+         * (need at least 2.5us to trigger usb disconnect)
+         */
+        gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
+                GPIO_CNF_OUTPUT_PUSHPULL, GPIO12);
+        gpio_clear(GPIOA, GPIO12);
+        for (unsigned int i = 0; i < 800000; i++) {
+                __asm__("nop");
+        }
+
+#else
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE,
+		GPIO9 | GPIO11 | GPIO12);
+	gpio_set_af(GPIOA, GPIO_AF10, GPIO9 | GPIO11 | GPIO12);
+#endif
+
+}
+
+void usb_cdcacm_setup_post_arch(void)
+{
+}
+
+// hacktastic
+extern struct ringb tx_ring;
+bool glue_send_data_cb(uint8_t *buf, uint16_t len)
+{
+	gpio_set(LED_TX_PORT, LED_TX_PIN);
+//	gpio_set(RS485DE_PORT, RS485DE_PIN);
+	for (int x = 0; x < len; x++) {
+		if (!ringb_put(&tx_ring, buf[x])) {
+			// oh shit. tx ring must be big enough for, what, 2 usb packets?
+			blocking_handler();
+		}
+	}
+	usart_enable_tx_interrupt(USART2);
+	// If we're over 64, we can't take another full packet.
+	return (ringb_depth(&tx_ring) > 64);
+}
+
+void glue_set_line_state_cb(uint8_t dtr, uint8_t rts)
+{
+	(void) dtr;
+	(void) rts;
+	// LM4f has an implementation of this if you're keen
+}
+
+int glue_set_line_coding_cb(uint32_t baud, uint8_t databits,
+	enum usb_cdc_line_coding_bParityType cdc_parity,
+	enum usb_cdc_line_coding_bCharFormat cdc_stopbits)
+{
+	int uart_parity;
+	int uart_stopbits;
+
+	if (databits < 8 || databits > 9) {
+		return 0;
+	}
+
+	/* Be careful here, ST counts parity as a data bit */
+	switch (cdc_parity) {
+	case USB_CDC_NO_PARITY:
+		uart_parity = USART_PARITY_NONE;
+		break;
+	case USB_CDC_ODD_PARITY:
+		uart_parity = USART_PARITY_ODD;
+		databits++;
+		break;
+	case USB_CDC_EVEN_PARITY:
+		uart_parity = USART_PARITY_EVEN;
+		databits++;
+		break;
+	default:
+		return 0;
+	}
+
+	switch (cdc_stopbits) {
+	case USB_CDC_1_STOP_BITS:
+		uart_stopbits = USART_STOPBITS_1;
+		break;
+	case USB_CDC_2_STOP_BITS:
+		uart_stopbits = USART_STOPBITS_2;
+		break;
+	default:
+		return 0;
+	}
+
+	/* Disable the UART while we mess with its settings */
+	usart_disable(USART2);
+	/* Set communication parameters */
+	usart_set_baudrate(USART2, baud);
+	usart_set_databits(USART2, databits);
+	usart_set_parity(USART2, uart_parity);
+	usart_set_stopbits(USART2, uart_stopbits);
+	/* Back to work. */
+	usart_enable(USART2);
+
+	return 1;
+}
